0af77ee7e32d7db638ba64609d6c078f0d076989
mrrk181 <raafayrashid@gmail.com>
2017-03-19 00:40:00 +0500

    Fixed a bug in methods missing "," Added Personnel Methods (upsert/remove)

--- base/imports/api/documents/methods.js
+++ base/imports/api/documents/methods.js
@@ -1,8 +1,8 @@
 import { SimpleSchema } from 'meteor/aldeed:simple-schema';
 import { ValidatedMethod } from 'meteor/mdg:validated-method';
 import Documents from './documents';
+import personnel from './personnel';
 import rateLimit from '../../modules/rate-limit.js';
-
 export const upsertDocument = new ValidatedMethod({
   name: 'documents.upsert',
   validate: new SimpleSchema({
@@ -32,4 +32,35 @@ rateLimit({
   ],
   limit: 5,
   timeRange: 1000,
+});
+
+export const upsertPerson = new ValidatedMethod({
+    name :'personnel.upsert',
+    validate : new SimpleSchema({
+        _id :{type:String},
+        name:{type :String},
+        armyNumber:{type:Number},
+        dob:{type:String},
+        rank:{type:String},
+        batchNo:{type: Number},
+        medCat:{type:String},
+        trade:{type:String},
+        currentUnit:{type:String},
+        milCourses:{type:[String]},
+        children:{type:[String]},
+        parents:{type:[String]}
+
+    }).validator(),
+    run(person){
+        return personnel.upsert({_id:person_id},{$set:person});
+    },
+});
+export const removePerson = new ValidatedMethod({
+  name: 'personnel.remove',
+  validate: new SimpleSchema({
+    _id: { type: String },
+  }).validator(),
+  run({ _id }) {
+    personnel1.remove(_id);
+  },
 });
--- base/imports/api/documents/server/publications.js
+++ base/imports/api/documents/server/publications.js
@@ -1,10 +1,15 @@
 import { Meteor } from 'meteor/meteor';
 import { check } from 'meteor/check';
 import Documents from '../documents';
+import personnel from '../personnel';
 
 Meteor.publish('documents.list', () => Documents.find());
+Meteor.publish('personnel.list', () => personnel.find());
 
 Meteor.publish('documents.view', (_id) => {
   check(_id, String);
   return Documents.find(_id);
+});
+Meteor.publish('personnel.view', (_id) => {
+  return personnel.find(_id);
 });
